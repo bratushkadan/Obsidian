44b78f03b87bd8aa0f2284480a8bdb53d3c26a00
bratushkadan <bratushkadan@gmail.com>
2024-11-18 09:22:23 +0300

    fix:add proper logging to ListRecursive implementation

--- pkg/fs/files.go
+++ pkg/fs/files.go
@@ -6,12 +6,12 @@ import (
 	"path"
 )
 
-func ListRecursive(dirPath string) ([]string, error) {
+func listRecursive(dirPath string) ([]string, error) {
 	var items []string
 
 	dirContents, err := os.ReadDir(dirPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to list directory: %w", err)
+		return nil, err
 	}
 
 	for _, dirent := range dirContents {
@@ -28,4 +28,12 @@ func ListRecursive(dirPath string) ([]string, error) {
 	}
 
 	return items, nil
+}
+
+func ListRecursive(dirPath string) ([]string, error) {
+	contents, err := listRecursive(dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list directory %s recursively: %w", dirPath, err)
+	}
+	return contents, err
 }
